07b63dcbd2d7e6db66dff40d1217b7c1a2a4bdd3
Azuki.I <azuking69@hotmail.co.jp>
2026-07-12 08:43:20 +0900

    26.07.12 made routing index of app.py

--- p00_flask_basics/app.py
+++ p00_flask_basics/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request
 
+#  １）アプリ作成
 app= Flask(__name__)
 
+#  ３）ルーティング
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html', name="AI Agent 수강생")
+
+#  ２）起動
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
